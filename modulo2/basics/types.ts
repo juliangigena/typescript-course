e98b735087acce58f4fe33d329e9f83e3d5b54d7
JulianGigena <juliangigena@MAC-68562.local>
2024-05-20 07:49:26 -0300

    2do commit

--- modulo2/basics/types.ts
+++ modulo2/basics/types.ts
@@ -26,3 +26,16 @@ let saludo2 : unknown;
 //varaibles vacias
 let saludo3 : null;
 let saludo4 : undefined;
+
+//variables de valor variable, no los manejo, por ej entran por teclado o por un svc
+let dinero : number | null; //por ej en una deuda me deben un monto o nada
+let deuda : number | undefined; // simil anterior, pero tengo mas libertad de funciones
+
+//variables centralizadas, ideal para proj grandes
+type responceService = string | undefined;
+let respuesta : responceService;
+
+//casteo de variable
+let sms : any = "";
+(sms as string).concat;
+let smsUpercase = <string>sms;
